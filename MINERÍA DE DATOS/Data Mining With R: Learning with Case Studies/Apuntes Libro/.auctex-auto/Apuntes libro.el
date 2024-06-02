5200182f8d82ef26ff806f32c993af56ff6a82f1
Ludwig <theludway@gmail.com>
2024-06-02 14:22:47 -0500

    Primer capítulo con R

--- MINERÍA DE DATOS/Data Mining With R: Learning with Case Studies/Apuntes Libro/.auctex-auto/Apuntes libro.el
+++ MINERÍA DE DATOS/Data Mining With R: Learning with Case Studies/Apuntes Libro/.auctex-auto/Apuntes libro.el
@@ -3,6 +3,16 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks") ("inputenc" "utf8") ("babel" "spanish") ("xcolor" "dvipsnames")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -32,6 +42,31 @@
     "multirow")
    (TeX-add-symbols
     '("HRule" 1)
-    "newline"))
+    "newline")
+   (LaTeX-add-thmtools-declaretheoremstyles
+    "thmsty"
+    "prosty"
+    "prcpsty")
+   (LaTeX-add-thmtools-declaretheorems
+    "theorem"
+    "proposition"
+    "principle")
+   (LaTeX-add-xcolor-definecolors
+    "LightGray"
+    "LightOrange"
+    "LightGreen"
+    "dkgreen"
+    "gray"
+    "mauve"
+    "codegreen"
+    "codegray"
+    "codepurple"
+    "backcolour")
+   (LaTeX-add-listings-lstdefinestyles
+    "RStyle")
+   (LaTeX-add-array-newcolumntypes
+    "L"
+    "C"
+    "R"))
  :latex)
 
