56d8f176eda46218bd864ac1bb451dab73ef2468
Ludwig <theludway@gmail.com>
2024-06-03 17:43:58 -0500

    Capítulo 2 minería de Datos

--- MINERÍA DE DATOS/INTRODUCCIÓN A LA MINERÍA DE DATOS/.auctex-auto/Apuntes Libro.el
+++ MINERÍA DE DATOS/INTRODUCCIÓN A LA MINERÍA DE DATOS/.auctex-auto/Apuntes Libro.el
@@ -4,15 +4,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("hyperref" "hidelinks") ("inputenc" "utf8") ("babel" "spanish") ("xcolor" "dvipsnames")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -44,7 +44,8 @@
     '("HRule" 1)
     "newline")
    (LaTeX-add-labels
-    "tab:datos-empleados")
+    "tab:datos-empleados"
+    "fig:data-warehouse")
    (LaTeX-add-thmtools-declaretheoremstyles
     "thmsty"
     "prosty"
