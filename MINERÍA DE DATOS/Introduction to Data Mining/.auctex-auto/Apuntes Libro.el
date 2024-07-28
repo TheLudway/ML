2dd295b880aecca62a73de03423d6e9dd1587ad0
Ludwig <theludway@gmail.com>
2024-07-28 14:52:27 -0500

    Capítulo 1

--- MINERÍA DE DATOS/Introduction to Data Mining/.auctex-auto/Apuntes Libro.el
+++ MINERÍA DE DATOS/Introduction to Data Mining/.auctex-auto/Apuntes Libro.el
@@ -7,6 +7,16 @@
                      '(("article" "")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("amsmath" "") ("amsthm" "") ("amssymb" "") ("amsfonts" "") ("thmtools" "") ("graphicx" "") ("setspace" "") ("geometry" "") ("float" "") ("hyperref" "hidelinks") ("inputenc" "utf8") ("babel" "spanish") ("framed" "") ("xcolor" "dvipsnames") ("tcolorbox" "") ("tikz" "") ("caption" "") ("longtable" "") ("pdflscape" "") ("svg" "") ("subcaption" "") ("multirow" "") ("array" "") ("listings" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -37,6 +47,25 @@
     "listings")
    (TeX-add-symbols
     '("HRule" 1)
-    "newline"))
+    "newline")
+   (LaTeX-add-thmtools-declaretheoremstyles
+    "thmsty"
+    "prosty"
+    "prcpsty")
+   (LaTeX-add-thmtools-declaretheorems
+    "theorem"
+    "proposition"
+    "principle")
+   (LaTeX-add-xcolor-definecolors
+    "LightGray"
+    "LightOrange"
+    "LightGreen")
+   (LaTeX-add-array-newcolumntypes
+    "L"
+    "C"
+    "R")
+   (LaTeX-add-listings-lstdefinestyles
+    "bashstyle"
+    "javastyle"))
  :latex)
 
